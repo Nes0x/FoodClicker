f763b302248e71066bccb478fa14121432722b58
Nes0x <66802265+Nes0x@users.noreply.github.com>
2021-08-23 14:30:44 +0200

    Update UpdateInformation.java

--- src/main/java/me/nes0x/UpdateInformation.java
+++ src/main/java/me/nes0x/UpdateInformation.java
@@ -17,20 +17,29 @@ public class UpdateInformation {
         BufferedInputStream bufferedInputStream = new BufferedInputStream(urlConnection.getInputStream());
 
 
-        int i = bufferedInputStream.read();
-        characterArrayList.add((char) i);
 
-        if (!characterArrayList.get(0).equals('3')) {
+        int i = 0;
+        while ((i = bufferedInputStream.read()) != -1) {
+            characterArrayList.add((char)i);
+        }
+
+        StringBuilder version = new StringBuilder();
+
+        for (Character charUpdate : characterArrayList) {
+            version.append(charUpdate);
+        }
+
+        if (!version.toString().equalsIgnoreCase("4.0")) {
             int result = JOptionPane.showConfirmDialog(
                     Clicker.getFrame(),
-                    "New version of FoodClicker! " + characterArrayList.get(0) + ".0" + "\nDo you want to update it?",
+                    "New version of FoodClicker! " + version + "\nDo you want to update it?",
                     "Update!",
                     JOptionPane.YES_NO_OPTION,
                     JOptionPane.QUESTION_MESSAGE
 
             );
             if (result == JOptionPane.YES_OPTION) {
-                java.awt.Desktop.getDesktop().browse(URI.create("https://github.com/Nes0x/FoodClicker/releases/tag/" + characterArrayList.get(0) + ".0"));
+                java.awt.Desktop.getDesktop().browse(URI.create("https://github.com/Nes0x/FoodClicker/releases/tag/" + version));
             }
 
         }
@@ -39,7 +48,3 @@ public class UpdateInformation {
 
     }
 }
-
-
-
-
